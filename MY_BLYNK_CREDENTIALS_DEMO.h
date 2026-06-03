29e3d4ff7f729e3a4ec15a4d9b2612d3f0b078b3
Thor Johnson <2821513+thorathome@users.noreply.github.com>
2026-06-03 15:57:13 -0400

    Add Blynk credentials and template definitions

---
+++ MY_BLYNK_CREDENTIALS_DEMO.h
@@ -0,0 +1,38 @@
+//#include "MY_BLYNK_CREDENTIALS.h"  // #defines MY_BLYNK_SERVER and MY_xxx_AUTHCODE
+
+#define MY_BLYNK_SERVER                            "blynk.cloud"                  // Common to all New Blynk sketches
+//                       IP address as of Nov 2023 64.225.16.22
+#ifndef BLYNK_FIRMWARE_VERSION
+  #define BLYNK_FIRMWARE_VERSION                     "1.2.3"
+#endif
+
+//#define BLYNK_USE_128_VPINS true // Gives codes for VPINS up to V127
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+  // These are dummy Template IDs and Device AuthcoXIAO des for demonstration
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+// ****************** XIAO 6-Channel Relay ***************** 
+// 10 //
+#define MY_XIAO_RELAY_TEMPLATE_ID                   "TMPL2aaaaaaaa"
+#define MY_XIAO_RELAY_TEMPLATE_NAME                 "XIAO Relay Template"
+#define MY_XIAO_RELAY_DEVICE_AUTHCODE               "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy" 
+// ADDED June 2026
+
+
+
+// ************** DUMMY TEMPLATE ************** 
+// 99 //
+// THESE ARE DUMMY SETTINGS FOR MY STARTUP TEMPLATE IN EXAMPLES
+#define MY_STARTUP_TEMPLATE_ID                      "XXXXXXXXXXXX"                // My Blynk Startup Template in Examples
+#define MY_STARTUP_TEMPLATE_NAME                    "Startup Template"
+#define MY_STARTUP_DEVICE_AUTHCODE                  "yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
+
+
+
+
